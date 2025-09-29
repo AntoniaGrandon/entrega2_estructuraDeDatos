475e5ef6b2433d408470209a580ed1d9b2384139
anto <aigrandon@miuandes.cl>
2025-09-29 15:56:44 -0300

    node

---
+++ poscodes/cpp/include/node.hpp
@@ -0,0 +1,19 @@
+#ifndef NODE_HPP_
+#define NODE_HPP_
+
+class Node {
+private:
+    size_t data;
+    Node* ptrNext;
+public:
+    Node();
+    Node(size_t _data, Node* next = nullptr);
+    void setData(size_t _data);
+    void setNext(Node* _next);
+    size_t getData();
+    Node* getNext();
+    void print();
+    virtual ~Node();
+};
+
+#endif
---
+++ poscodes/cpp/src/node.cpp
@@ -0,0 +1,31 @@
+#include "lLists/node.hpp"
+#include <iostream>
+
+Node::Node(): data(0), ptrNext(nullptr) {
+}
+
+Node::Node(size_t val, Node* next): data(val), ptrNext(next) {
+}
+
+void Node::setData(size_t _data){
+    data = _data;
+}
+
+void Node::setNext(Node* next){
+    ptrNext = next;
+}
+
+size_t Node::getData(){
+    return data;
+}
+
+Node* Node::getNext(){
+    return ptrNext;
+}
+
+void Node::print(){
+    std::cout << data ;
+}
+
+Node::~Node() {
+}
