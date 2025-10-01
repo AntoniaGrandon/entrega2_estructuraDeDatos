f6bde34e74d2ca17e3ca6f2e431734734a934c40
anto <aigrandon@miuandes.cl>
2025-10-01 20:25:02 -0300

    listo

--- poscodes/python/grafico.py
+++ poscodes/python/grafico.py
@@ -3,12 +3,12 @@ import numpy as np
 
 sizes_data = [500000, 1000000, 10000000]
 
-radix_means = [786.15, 2420.14, 30545.00]
-quick_means = [331.74, 648.57, 9818.94]
-merge_means = [541.55, 864.32, 20726.60]
+radix_means = [928.401, 2107.36, 29852.7]
+quick_means = [243.514, 518.198, 6581.54]
+merge_means = [477.965, 1639.68, 14149.7]
 
-x_ticks_values = [10000, 100000, 500000, 1000000, 10000000]
-x_ticks_labels = ['10K', '100K', '500K', '1M', '10M']
+x_ticks_values = [500000, 1000000, 10000000]
+x_ticks_labels = ['500K', '1M', '10M']
 
 fig, ax = plt.subplots(figsize=(12, 8))
 
@@ -19,7 +19,7 @@ ax.set_xscale('log')
 ax.set_yscale('log')
 ax.set_ylabel('Tiempo de Ejecución (ms)')
 ax.set_xlabel('Tamaño del Dataset')
-ax.set_title('Comparación de Rendimiento de Algoritmos (Escala Log-Log)')
+ax.set_title('Comparación de Rendimiento de Algoritmos')
 ax.legend()
 ax.grid(True, which="both", ls="--")
 ax.set_xticks(x_ticks_values)
